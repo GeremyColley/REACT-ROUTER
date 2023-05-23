cc7a325c9e31873d0df4fd29ad94075fc5196201
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-05-23 17:26:28 +0200

    third commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,12 @@ const Header = () => {
     console.log(location.pathname);
     return (
         <div>
+            <h2>{location.pathname}</h2>
+            {location.pathname === "/" ? null :(
+                <div>
+                    <h1>barre de menu</h1>
+                </div>
+            )}
             <button onClick={() => {navigate("/");}}>Home</button>
             <button onClick={() => {navigate("/about");}}>About</button>
             <button onClick={() => {navigate("/contact");}}>contact</button>
