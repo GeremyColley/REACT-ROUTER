12c8ed76b1ea297accacde98df9d24d1a37c3592
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-05-24 12:23:28 +0200

    commit de branch

---
+++ src/components/Body.jsx
@@ -0,0 +1,10 @@
+const Body = () => {
+
+    return (
+        <div>
+            <p>branch</p>
+        </div>
+    );
+};
+
+export default Body;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,11 @@
 import { useNavigate } from "react-router-dom";
+import Body from "../components/Body";
 
 const Contact = () => {
     return (
         <div>
             <h1>Contact</h1>
+            <Body />
         </div>
     );
 };
